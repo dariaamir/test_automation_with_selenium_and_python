f1c977342ccca11412f94f5a9b0d4e742256a2d3
dariaamir <19853206+dariaamir@users.noreply.github.com>
2023-07-07 11:26:13 +0300

    Added checks, that login page is opened

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,15 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
 def test_guest_should_see_login_link(browser):
    page = MainPage(browser, link)
    page.open()
